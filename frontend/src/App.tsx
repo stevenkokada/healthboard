1a56c560e7f0b4c7d535238244706be4a21eeecb
Steven <stevenkokada@gmail.com>
2022-04-16 17:01:46 -0700

    frontend: WOOT can now fetch from django server on ec2
    ec2 instance ip is hardcoded in right now but should be sourced from environment variable in cloudflare pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  useEffect(() => {
+    fetch("http://ec2-18-144-155-199.us-west-1.compute.amazonaws.com:8000")
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  });
+
   return (
     <div className="App">
       <header className="App-header">
