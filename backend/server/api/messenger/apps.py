97177e54a5e355412b60cf4919516bd72b45a82c
Steven <stevenkokada@gmail.com>
2022-04-17 21:08:35 -0700

    require api.messenger models to build app, create migration

--- backend/server/api/messenger/apps.py
+++ backend/server/api/messenger/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class QuickstartConfig(AppConfig):
+class MessengerAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'messenger'
+    name = 'api.messenger'
