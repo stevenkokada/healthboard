47fca14eff019551eb29a62969f2b5f3a81023ce
Steven <stevenkokada@gmail.com>
2022-04-17 23:26:02 -0700

    frontend: add logic to display unread messages within certain window

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 import "./App.css";
 
 interface UnreadMesssages {
-  count: number;
   messages: Array<any>;
 }
 
@@ -11,33 +10,49 @@ function App() {
   useEffect(() => {
     // DJANGO RESOURCES NEED TRAILING SLASH FOR SOME GODDAMN REASON
     // PROD_BACKEND - LEAVE THIS ENABLED BY DEFAULT SINCE THIS GETS SERVED until we flag on env vars
-    fetch("https://okada-api.com/unread_messages/")
-      .then((response) => response.json())
-      .then((data) =>
-        setUnreadMessages({ count: data.count, messages: data.results })
-      );
-    // LOCAL_BACKEND
-    // fetch("http://127.0.0.1:8000/unread_messages/")
+    // fetch("https://okada-api.com/unread_messages/")
     //   .then((response) => response.json())
     //   .then((data) =>
-    //     setUnreadMessages({ count: data.count, messages: data.results })
+    //     setUnreadMessages({messages: data.results })
     //   );
+    // LOCAL_BACKEND
+    fetch("http://127.0.0.1:8000/unread_messages/")
+      .then((response) => response.json())
+      .then((data) => {
+        setUnreadMessages({ messages: data.results });
+      });
   });
 
-  if (unreadMessages && unreadMessages.count == 0) {
+  if (unreadMessages && unreadMessages?.messages?.length == 0) {
     return <>No Message Data</>;
   }
 
-  const messages = unreadMessages?.messages?.map((message) => {
-    return (
-      <div>
-        {`${message.sender_name}` + " " + `${message.scraper_run_time}`}
-      </div>
-    );
+  const current_time_ms = Math.floor(Date.now() / 1000);
+  const window = 2500; // look back for unread messages in past 120 seconds
+
+  const filteredMessages = unreadMessages?.messages?.filter((message) => {
+    if (message.scraper_run_time > current_time_ms - window) {
+      return true;
+    }
   });
+  const unreadCount = filteredMessages?.length;
+
   // console.log(unreadMessages);
 
-  return <>{messages}</>;
+  return (
+    <>
+      <div>{`${unreadCount} unread messages in past ${window} seconds`}</div>
+      <div>
+        {filteredMessages?.map((message) => {
+          return (
+            <div>
+              {`${message.sender_name}` + " " + `${message.scraper_run_time}`}
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
 }
 
 export default App;
