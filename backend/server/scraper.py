c889afd30496cf2026dfe369afbd3fc015324db0
Steven <stevenkokada@gmail.com>
2022-04-30 16:04:30 -0700

    pass in env file to scraper

--- backend/server/scraper.py
+++ backend/server/scraper.py
@@ -39,8 +39,9 @@ while True:
     time.sleep(30)
     scraped_messages = set()
     unread_count = 0
-    print("number of chats: ", len(unread_chat_names_elts))
     unread_chat_names_elts = driver.find_elements(By.XPATH, '//*[@id="jsc_c_c"]/div/div/div[*]/div/div[1]/a/div[1]/div/div[2]/div/div/span/span')
+    print("number of chats: ", len(unread_chat_names_elts))
+
     for chat_name_elt in unread_chat_names_elts:
         try:
             if int(chat_name_elt.value_of_css_property('font-weight')) > 500:
